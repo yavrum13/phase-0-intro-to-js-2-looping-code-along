d0232f4181450f334caab5e1d0b8fa1e122eaf6a
yavrum13 <yuval.avrum@gmail.com>
2024-01-20 23:41:12 -0600

    This was really hard honestly

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards, surprise) {
+    let thankYou = [];
+    for (let i = 0; i < cards.length; i++){
+        let x = `Thank you, ${cards[i]}, for the wonderful surprise gift!`;
+        thankYou.push(x);
+    }
+    return thankYou;
+}
+
+function countDown(startingNumber){
+    let currentNumber = startingNumber;
+    while (currentNumber>=0){
+        console.log(currentNumber);
+        currentNumber--;
+    }
+}
